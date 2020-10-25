f23d5ae7a441ac0def094f483a33c02201fd94c2
pong34976 <38376640+pong34976@users.noreply.github.com>
2020-10-26 06:56:44 +0700

    Update sss.php

--- sss.php
+++ sss.php
@@ -36,9 +36,9 @@ if (!is_null($events['events'])) {
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
 			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, "xxx");
+			curl_setopt($ch, CURLOPT_POSTFIELDS, "ssssxxxx".$post);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
