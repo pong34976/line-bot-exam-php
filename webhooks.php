148e3a8b3aa2fc68cf632567f2d20e0da29bbb07
pong34976 <38376640+pong34976@users.noreply.github.com>
2020-10-25 20:00:02 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -43,9 +43,9 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
 			curl_close($ch);
-                       print_r($post);
+                      
 			echo $result . "\r\n";
 		}
 	}
 }
-echo "OK";
+echo "OKx";
