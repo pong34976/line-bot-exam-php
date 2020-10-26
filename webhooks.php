9f5069b48f787cf803404a46a65051fe72052bca
pong34976 <38376640+pong34976@users.noreply.github.com>
2020-10-26 07:17:15 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -23,7 +23,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				"text":"Hello, user"
+				"text" => "Hello, user"
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
