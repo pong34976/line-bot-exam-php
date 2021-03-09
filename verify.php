3c108aa64acc4d140a6a2d7b7a05331eddbb9d06
pong34976 <38376640+pong34976@users.noreply.github.com>
2021-03-09 12:30:35 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,35 +1,9 @@
-<?php
-// Access Token
-$access_token = 'a40f6b9359e38362e37992cedc88e6f9';
-// รับค่าที่ส่งมา
-$content = file_get_contents('php://input');
-// แปลงเป็น JSON
-$events = json_decode($content="", true);
-if (!empty($events['events'])) {
-    foreach ($events['events'] as $event) {
-        if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-            // ข้อความที่ส่งกลับ มาจาก ข้อความที่ส่งมา
-            // ร่วมกับ USER ID ของไลน์ที่เราต้องการใช้ในการตอบกลับ
-            $messages = array(
-                'type' => 'text',
-                'text' => 'Reply message : '.$event['message']['text']."\nUser ID : ".$event['source']['userId'],
-            );
-            $post = json_encode(array(
-                'replyToken' => $event['replyToken'],
-                'messages' => array($messages),
-            ));
-            // URL ของบริการ Replies สำหรับการตอบกลับด้วยข้อความอัตโนมัติ
-            $url = 'https://api.line.me/v2/bot/message/reply';
-            $headers = array('Content-Type: application/json', 'Authorization: Bearer '.$access_token);
-            $ch = curl_init($url);
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-            $result = curl_exec($ch);
-            curl_close($ch);
-            echo $result;
-        }
-    }
-}
+<?php 
+  $access_token = '2bLPW2Xj95hxP/LqNAm7ewu70ybbL1dYEqAEjoapl5RBdfumPOuSCaB0A1Pq4bnpxfenJMP96ldhnNfPlnqqmvjPlVzjkRLtUmta/XRO7KqS5VzytSMc9sE2BLC/l7H8heCLMb58O53yZGUDnWij3AdB04t89/1O/w1cDnyilFU=';
+  $url = 'https://api.line.me/v1/oauth/verify';
+  $headers = array('Authorization: Bearer ' . $access_token);
+  $ch = curl_init($url);curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  $result = curl_exec($ch);curl_close($ch);
+  echo $result;
