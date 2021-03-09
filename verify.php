7d3680580e88d5c21d69e66a93c8867729143c9b
pong34976 <38376640+pong34976@users.noreply.github.com>
2021-03-09 13:35:58 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,6 +1,16 @@
 <?php 
   $access_token = '2bLPW2Xj95hxP/LqNAm7ewu70ybbL1dYEqAEjoapl5RBdfumPOuSCaB0A1Pq4bnpxfenJMP96ldhnNfPlnqqmvjPlVzjkRLtUmta/XRO7KqS5VzytSMc9sE2BLC/l7H8heCLMb58O53yZGUDnWij3AdB04t89/1O/w1cDnyilFU=';
- $url = 'https://api.line.me/v2/bot/message/reply';
+    // ข้อความที่ส่งกลับ มาจาก ข้อความที่ส่งมา
+            // ร่วมกับ USER ID ของไลน์ที่เราต้องการใช้ในการตอบกลับ
+            $messages = array(
+                'type' => 'text',
+                'text' => 'Reply message : '.$event['message']['text']."\nUser ID : ".$event['source']['userId'],
+            );
+            $post = json_encode(array(
+                'replyToken' => $event['replyToken'],
+                'messages' => array($messages),
+            ));
+$url = 'https://api.line.me/v2/bot/message/reply';
             $headers = array('Content-Type: application/json', 'Authorization: Bearer '.$access_token);
             $ch = curl_init($url);
             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
