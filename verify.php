5d22672b72c5afc07feb4ab800ff0de8b1dd0ef0
pong34976 <38376640+pong34976@users.noreply.github.com>
2021-03-09 15:02:35 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -33,10 +33,9 @@
      if($message == "ขอid"){
        $arrayPostData['to'] = $id;
       $arrayPostData['messages'][0]['type'] = "text";
-      $arrayPostData['messages'][0]['text'] = "รกของคุณคือ \n ".$idu;
-      /*$arrayPostData['messages'][1]['type'] = "sticker";
-      $arrayPostData['messages'][1]['packageId'] = "2";
-      $arrayPostData['messages'][1]['stickerId'] = "34"; */
+      $arrayPostData['messages'][0]['text'] = "รกของคุณคือ ...";
+        $arrayPostData['messages'][1]['type'] = "text";
+      $arrayPostData['messages'][1]['text'] =  $idu;
       pushMsg($arrayHeader,$arrayPostData);
    }
    function pushMsg($arrayHeader,$arrayPostData){
