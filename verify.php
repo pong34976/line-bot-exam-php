7b74d20514a16b86290ea8439ab64a5cd7b0fe50
pong34976 <38376640+pong34976@users.noreply.github.com>
2021-03-16 08:43:39 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -30,6 +30,15 @@
       $arrayPostData['messages'][1]['stickerId'] = "34"; */
       pushMsg($arrayHeader,$arrayPostData);
    }
+  if($message == "test"){
+       $arrayPostData['to'] = $idu;
+      $arrayPostData['messages'][0]['type'] = "text";
+      $arrayPostData['messages'][0]['text'] = $message;
+      /*$arrayPostData['messages'][1]['type'] = "sticker";
+      $arrayPostData['messages'][1]['packageId'] = "2";
+      $arrayPostData['messages'][1]['stickerId'] = "34"; */
+      pushMsg($arrayHeader,$arrayPostData);
+   }
      if($message == "ขอid"){
        $arrayPostData['to'] = $idu;
       $arrayPostData['messages'][0]['type'] = "text";
